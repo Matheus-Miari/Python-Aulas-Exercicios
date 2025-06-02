1c31f4084ccc385299f607fac532532433d2e1f3
Matheus Miari <matmiari.eng@gmail.com>
2025-06-02 11:12:01 -0300

    Add files via upload

---
+++ Aulas/aula010.py
@@ -0,0 +1,14 @@
+# condicoes
+
+# if =  se     // utilizar :  e identacao para dentro  com 4 espacos
+# else = se nao
+
+"""
+nome = str(input('Qual seu nome: ')).strip().upper()
+if nome == 'BATATA':
+    print(f'Prazer, Sr {nome}')
+else:
+    print('Seu nome deveria ser Batata')
+"""
+
+
