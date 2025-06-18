6d23689c9b0f567e58168e4502111a2f59549855
Matheus Miari <matmiari.eng@gmail.com>
2025-06-18 10:45:57 -0300

    Add files via upload

--- Exercicios/ex057.py
+++ Exercicios/ex057.py
@@ -1,7 +1,7 @@
 # faca um programa que leia sexo de uma pessoa
 # mas so aceite valores M ou F #
 
-sex = input('Sexo [M/F]: ').upper().strip()
+sex = input('Sexo [M/F]: ').upper().strip()[0]
 while sex not in ['M', 'F']:
-    sex = input('Valor inválido. Sexo [M/F]: ').upper().strip()
-print('Sexo registrado com sucesso!')
+    sex = input('Valor inválido. Sexo [M/F]: ').upper().strip()[0]
+print(f'Sexo {sex} registrado com sucesso!')
--- Exercicios/ex063.py
+++ Exercicios/ex063.py
@@ -3,17 +3,17 @@
 # de uma sequencia de fibonacci#
 
 
-n = int(input('Quantos termos você quer mostrar? '))
-t1 = 0
-t2 = 1
+n = int(input('Quantos termos você quer mostrar? ')) # quantos termos
+t1 = 0   # termo 1 comeca com 0
+t2 = 1   # termo 2 comeca com 1
 print(f'{t1} → {t2}', end='')
 contador = 3
-while contador <= n:
-    t3 = t1 + t2
-    print(f' → {t3}', end='')
-    t1 = t2
-    t2 = t3
-    contador += 1
+while contador <= n:  # comeca a contagem ate atingir a meta
+    t3 = t1 + t2   # termo3  = a t1 + t2
+    print(f' → {t3}', end='')  # printa o termo
+    t1 = t2     # t1 assume a posicao da frente
+    t2 = t3   # assume a posicao do t3
+    contador += 1   # contador
 
 print(' → FIM')
 print('~' * 30)
