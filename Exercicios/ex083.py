ad9b7a795b85b9d9438335a497e0bd85cd7df893
Matheus Miari <matmiari.eng@gmail.com>
2025-06-30 14:09:46 -0300

    Add files via upload

--- Exercicios/ex083.py
+++ Exercicios/ex083.py
@@ -3,3 +3,20 @@
 # se a expressao passada esta com parenteses abertos e fechdos
 # na ordem correta
 
+exp = str(input('Digite a expressao: '))
+
+pilha = []
+
+for simb in exp:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Valida')
+else:
+    print('Invalida')
